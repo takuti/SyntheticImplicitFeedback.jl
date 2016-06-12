83eecb0f54a256251ebe7b0ccce116677a4eed88
Takuya Kitazawa <k.takuti@gmail.com>
2016-06-12 13:31:47 +0900

    implement simple generator

--- src/SyntheticImplicitFeedback.jl
+++ src/SyntheticImplicitFeedback.jl
@@ -1,5 +1,27 @@
 module SyntheticImplicitFeedback
 
-# package code goes here
+export Rule, generate
+
+type Rule
+    # return bool
+    f::Function
+
+    # if f returns true, accumulative CTR is lifted p%
+    p::Float64
+end
+
+function generate(samples::Array{Dict,1}, rules::Array{Rule,1})
+    feedback = Bool[]
+    for sample in samples
+        ctr = 0.0
+        for rule in rules
+            if rule.f(sample)
+                ctr += rule.p
+            end
+        end
+        push!(feedback, rand() <= ctr)
+    end
+    feedback
+end
 
 end # module
